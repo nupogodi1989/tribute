c029e5ce26adfcaf04377a8cbf0324627d8d8a37
T <coffeestain@protonmail.com>
2017-02-22 00:24:57 -0500

    Change css styling and add more description to initial paragraph

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $(this).html("<img style='width: 373.3333333333333px; height: 280px;' src='./resources/images/alexander_shulgin.jpg'>");
     },
     "mouseout" : function() {
-        $(this).html("<h1 class='text-center psychedelic-purple' id='heading'>Alexander Shulgin</h1>");
+        $(this).html("<h1 class='text-center' id='heading'>Alexander Shulgin</h1>");
     }
   });
 });
