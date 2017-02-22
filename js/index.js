511dc8f730754ab1e0c09a97fff25f6fecafe2c7
T <coffeestain@protonmail.com>
2017-02-22 00:07:39 -0500

    Add mouseover and mouseout functions for h1 title to switch to picture

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $("#heading").addClass("animated slideInDown");
+  //$("#heading").addClass("animated slideInDown");
+  $("#heading").on({
+    "mouseover" : function() {
+        $(this).html("<img style='width: 373.3333333333333px; height: 280px;' src='./resources/images/alexander_shulgin.jpg'>");
+    },
+    "mouseout" : function() {
+        $(this).html("<h1 class='text-center psychedelic-purple' id='heading'>Alexander Shulgin</h1>");
+    }
+  });
 });
